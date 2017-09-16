7c7f8e1560cbc49eb6f9bf1355a6015e6f95e722
antonioasaro <antonio_asaro@hotmail.com>
2017-09-16 08:00:17 -0400

    Changed url to TIME_SERIES_DAILY

--- server/stockprice.php
+++ server/stockprice.php
@@ -3,13 +3,13 @@
   if (!isset($_GET['stock'])) die();
   $stock = $_GET['stock'];
 
-  $url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMD&interval=15min&utputsize=compact&apikey=07MCZQ4M624HQ9TW";
+  $url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMD&interval=15min&utputsize=compact&apikey=07MCZQ4M624HQ9TW";
   $contents   = file_get_contents($url);
   $dataarray  = json_decode($contents, true);
-  $series     = json_encode($dataarray['Time Series (15min)']);
+  $series     = json_encode($dataarray['Time Series (Daily)']);
   $datafields = split(":", $series);
-  $closestart = $datafields[6+0];
-  $closeend   = $datafields[6+1];
+  $closestart = $datafields[4+0];
+  $closeend   = $datafields[4+1];
   $closeprice = split(",", $closeend);
 
   $stockprice = substr(str_replace("\"", "", $closeprice[0]), 0, -2);
